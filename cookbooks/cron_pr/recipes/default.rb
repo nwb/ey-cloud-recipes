8de9533199af51d6f2f64fbaf6e13af30c46b2a9
Adam van den Hoven <adamvandenhoven@naturalwellbeing.com>
2012-10-25 15:10:15 -0700

    master and slave add cron job to pull power reivew seo

--- cookbooks/cron_pr/recipes/default.rb
+++ cookbooks/cron_pr/recipes/default.rb
@@ -1,12 +1,14 @@
-if node[:instance_role] == 'app' 
-  cron "task_pull_pr_seo" do 
-    day '*/1' 
-    user 'deploy' 
-    command "/data/nwbstaging/current && rake production spree:extensions:power_reviews:load_data" 
-  end 
-end 
-
 #
 # Cookbook Name:: cron_pr
 # Recipe:: default
 #
+
+if ['app_master', 'app'].include?(node[:instance_role])
+#if node[:instance_role] == 'app_master' 
+  cron "task_pull_pr_seo" do 
+    hour '1'
+    day '*/1' 
+    user 'deploy' 
+    command "cd /data/nwbsite/current && rake production spree:extensions:power_reviews:load_data" 
+  end 
+end 
